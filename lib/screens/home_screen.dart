cf79970e0aa450a7b0fc1b655eadcdedd410d96d
antrimo <kartikeyai246@gmail.com>
2025-03-15 22:33:14 +0530

    feature: navbar structure

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,23 +1,35 @@
 import 'package:flutter/material.dart';
+import '../widgets/bottom_nav_bar.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+
+  int _selectedTab = 0;
+  final List<Widget> _screens = [
+    const Placeholder(),
+    const Placeholder(),
+    const Placeholder(),
+  ];
+
+  void _onTabChange(int index) {
+    setState(() {
+      _selectedTab = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Center(child: Text('Meme Generator')),
-      ),
-      body: const Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children:[
-            Text(
-              'Welcome to the Meme Generator!',
-            ),
-          ],
-        ),
+      body: _screens[_selectedTab],
+      bottomNavigationBar: BottomNavBar(
+        selectedIndex: _selectedTab,
+        onTabChange: _onTabChange,
       ),
     );
   }
